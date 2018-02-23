1ccc9bef3b4cee6442560380d8ffa7ef6539ce46
Chris Holdgraf <choldgraf@berkeley.edu>
2018-02-22 21:19:41 -0800

    updating example repos

--- doc/parse_readmes.py
+++ doc/parse_readmes.py
@@ -13,18 +13,24 @@ from github import Github
 import os
 
 EXCLUDE_FILES = ['.gitignore', 'LICENSE', 'README.md']
+EXCLUDE_REPOS = ['dockerfile-rstudio', 'zero-to-binder']
+
 # First create a Github instance:
 g = Github(os.environ['GITHUB_USERNAME'],
            os.environ['GITHUB_PASSWORD'])
 
 org = g.get_organization('binder-examples')
 repos = list(org.get_repos())
+repos = [repo for repo in repos if repo.name not in EXCLUDE_REPOS]
 
 lines = ['# Sample Binder Repositories\n',
          '\n'
          'Below we list several sample Binder repositories that\n'
          'demonstrate how to compose build files in order to create\n'
-         'Binders with varying environments.\n\n']
+         'Binders with varying environments. You can find all of the\n'
+         'repositories listed on this page at the\n'
+         '[binder-examples GitHub organization](https://github.com/binder-examples).\n\n']
+
 for i_repo, repo in enumerate(repos):
     print('{}/{}'.format(i_repo+1, len(repos)))
 
@@ -56,12 +62,12 @@ for i_repo, repo in enumerate(repos):
 
     # Append to doc file
     lines += readme_text
-    lines.append('### Files')
+    lines.append('### Files in this repository')
     lines.append('```')
     for this_file in file_names:
         lines.append(this_file)
     lines.append('```')
-    lines.append('```eval_rst\n|\n\n```')
+    lines.append('```eval_rst\n|\n|\n\n```')
 
 lines = [ln + '\n' for ln in lines]
 
