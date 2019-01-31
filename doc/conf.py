cefabf9b2852db1e1b826deff79287e835060cfe
Lindsey Heagy <lindseyheagy@gmail.com>
2019-01-31 15:04:16 -0800

    remove space in html theme options dict

--- doc/conf.py
+++ doc/conf.py
@@ -96,7 +96,7 @@ html_theme_path = [alabaster_jupyterhub.get_html_theme_path()]
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = { }
+# html_theme_options = {}
 
 html_context = {
     "github_user": "jupyterhub",
